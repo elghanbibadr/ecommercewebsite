5d6e4a97d6f48cf97a3690858c41478b1d8c5b30
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 19:56:33 +0100

    add the list of sizes available for all product

--- src/componenet/UI/ProductIntroCard.jsx
+++ src/componenet/UI/ProductIntroCard.jsx
@@ -4,15 +4,24 @@ import heartIcon from "../../assets/heart.svg";
 import addtobasket from "../../assets/addToBasket.svg"
 
 const ProductIntroCard = () => {
+    const availableSizes=["xs","s","md","l","xl","xxl"];
+
+    
+    const handleProuctGotHovered=()=>{
+        //show the available sizes for this  product
+    }
   return (
-    // create an img html element that hold the product image and add a displayed absolute new text if the item is new and a heart to add to wishlist
-    <div  className='relative w-[400px] m-4'>
+    <>
+    <div>
+
+    </div>
+    <div onMouseOver={productGotHovered} className='relative w-[400px] m-4'>
       <div className='bg-white p-2 rounded-full absolute left-[90%] top-3 '>
         <img className='w-4 h-4 lg:h-4 lg:w-4' src={heartIcon} alt="heart icon" />
         </div>  
       <div className='bg-white p-2 rounded-full absolute left-[5%] top-3 '>
         <img className='w-4 h-4 lg:h-3 lg:w-3' src={addtobasket} alt="add to basket icon" />
-        </div>  
+        </div>    
         <div className='bg-white rounded-sm p-1 absolute left-2 top-[78%]'>
             <p className='font-semibold text-xs'>NEW</p>
         </div>
@@ -21,7 +30,9 @@ const ProductIntroCard = () => {
        <span className='text-[#6e6e6e] my-1 inline-block '> Black</span>
         <h6 className='font-bold'> $30</h6>
     </div>
-  )
+    
+    </>
+  ) 
 }
 
 export default ProductIntroCard   
