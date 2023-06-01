4a364335c89bc6147713356a1a36abde4fc8506a
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 15:07:34 +0100

    add the menu hamurger

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import menuHamburger from "../assets/bars-solid.svg"
 const Navbar = () => {
   return (
     <header className='p-4'>
-        <nav>
+        <nav className='flex justify-between'>
+            <img className='h-6 w-6' src={menuHamburger} alt="menu hamburger icon" />
         <img src="https://cdn.gymshark.com/images/branding/gs-icon-black.svg" alt="store logo" /> 
         </nav>
     </header>
