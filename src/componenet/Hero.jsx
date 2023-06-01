349eddc585201de917cd4c99879cf0953e950edd
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 18:05:14 +0100

    make the button fit for all width

--- src/componenet/Hero.jsx
+++ src/componenet/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
     <div className='main -z-30'>
     <div className="overlay"></div>
     <video src={storeVideo} autoPlay loop muted />
-    <div className="content p-10">
+    <div className="content p-4 lg:p-10">
     <h2 className=' text-2xl lg:text-4xl font-medium'>LIMITLESS. THE NEW APEX</h2>
         <p className='my-3 text-sm lg:text-md font-medium'>Find that extra 1% with Limitless’s breathable, sweat-wicking and cool-touch tech.</p>
-        <div className='flex items-center relative right-2'>
-            <Btn className="bg-white mx-2 text-black" text="shop new releases" />
+        <div className='flex items-center w-full relative right-2'>
+            <Btn className="bg-white mx-2 w-full sm:w-auto text-black" text="shop new releases" />
                  <Btn className="bg-[#53565a] hidden lg:block text-white" text="shop 315" />
         </div>
     </div>
