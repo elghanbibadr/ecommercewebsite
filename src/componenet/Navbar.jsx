a793e63da85eaa19f52535515968be61782e5bcf
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 16:16:08 +0100

    create the list of available products links

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -12,11 +12,17 @@ const Navbar = () => {
   return (
     <header className='p-6 border-b-[#ccc] pb-4 border-b-[1px] lg:px-20'>
         <nav className='flex  justify-between items-center'>
-            <div className='flex items-center lg:hidden'>
+            <div className='flex items-center lg:hidden' >
                 <img className='h-5 mx-3 lg:hidden w-6' src={menuHamburger} alt="menu hamburger icon" />
                  <img className='h-4 lg:hidden' src={searchIcon} alt="search icon" />
             </div>
         <img src={logo} alt="store logo" />  
+        {/* dropdown menu  */}
+        <ul className=' hidden  lg:flex uppercase ml-40 font-semibold  items-center '>
+         <li>women</li>
+         <li className='mx-5'>men</li>
+         <li>accessories</li>
+        </ul>
          <div className='flex items-center'>  
          <img className=' hidden h-5 ml-2 lg:block lg:mx-6' src={searchIcon} alt="search icon" />
          <img className='  w-4 h-4 lg:h-5 lg:w-5' src={heartIcon} alt="heart icon" />
