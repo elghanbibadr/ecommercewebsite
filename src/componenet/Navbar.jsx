dc76e7ca46a01cfa9ec9a162acc74498f76737a0
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 15:11:41 +0100

    create the search icon

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import menuHamburger from "../assets/bars-solid.svg"
+import searchIcon from "../assets/searchIcon.svg"
+
 const Navbar = () => {
   return (
     <header className='p-4'>
         <nav className='flex justify-between'>
-            <img className='h-6 w-6' src={menuHamburger} alt="menu hamburger icon" />
+            <div className='flex h-6 w-6 items-center'>
+                <img className='h-6 mx-2 w-6' src={menuHamburger} alt="menu hamburger icon" />
+                 <img src={searchIcon} alt="search icon" />
+            </div>
         <img src="https://cdn.gymshark.com/images/branding/gs-icon-black.svg" alt="store logo" /> 
         </nav>
     </header>
