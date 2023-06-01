8773eecab8312f1d3e8e8be3c5709591c7c07ba2
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 21:24:59 +0100

    creating some some of the product data

---
+++ src/Data.js
@@ -0,0 +1,37 @@
+const newRealase=[
+  {
+    id:'1',
+    isNew: true,
+    title:'Elevate Spray Dye Leggings',
+    colors :"White/Misty Pink/Scandi Pink",
+    price:"$68"
+  },
+  {
+//     Elevate Spray Dye Longline Sports Bra
+// Light Support
+
+// White/Misty Pink/Scandi Pink
+
+// $46
+    id:'2', 
+    isNew: true,
+    title:'Elevate Spray Dye Longline Sports Bra',
+    colors :"White/Misty Pink/Scandi Pink", 
+    price:"$46"
+  },
+  {
+
+    id:'3',
+    isNew: true,      
+    title:'limitless Shorts',
+    colors :"Black/light grey",
+    price:"$48"
+  },
+  {
+    id:'4',
+    isNew: true,
+    title:'limitless Sport Bra',
+    colors :"Black",
+    price:"$58"
+  },
+]    
--- src/componenet/UI/ProductIntroCard.jsx
+++ src/componenet/UI/ProductIntroCard.jsx
@@ -4,41 +4,51 @@ import heartIcon from "../../assets/heart.svg";
 import addtobasket from "../../assets/addToBasket.svg"
 
 const ProductIntroCard = () => {
-    const [productHovered ,setProductHovered]=useState(false)
-     
-    const availableSizes=["xs","s","md","l","xl","xxl"];
+    const [productHovered, setProductHovered] = useState(false)
+
+    const availableSizes = ["xs", "s", "md", "l", "xl", "xxl"];
 
 
-    const handleProuctGotHovered=()=>{
-        //show the available sizes for this  product
+    const handleProuctGotHovered = () => {
+        //show the available sizes for this  product  
+        setProductHovered(true)  
     }
-  return (
-    <>
-    <div>
-    {availableSizes.map((size,index) => {
-        return <div className='bg-white hover:bg-black' key={index} >
-         <p className='text-[#6e6e6e]'>{size}</p> 
-        </div>
-    })}  
-    </div>
-    <div onMouseOver ={handleProuctGotHovered} className='relative w-[400px] m-4'>
-      <div className='bg-white p-2 rounded-full absolute left-[90%] top-3 '>
-        <img className='w-4 h-4 lg:h-4 lg:w-4' src={heartIcon} alt="heart icon" />
-        </div>  
-      <div className='bg-white p-2 rounded-full absolute left-[5%] top-3 '>
-        <img className='w-4 h-4 lg:h-3 lg:w-3' src={addtobasket} alt="add to basket icon" />
-        </div>    
-        <div className='bg-white rounded-sm p-1 absolute left-2 top-[78%]'>
-            <p className='font-semibold text-xs'>NEW</p>
-        </div>
-        <img className='' src={womenImg} alt="a guy modeling the product" />
-       <h4 className='mt-3 text-md '>Legacy Washed Crop Top</h4> 
-       <span className='text-[#6e6e6e] my-1 inline-block '> Black</span>
-        <h6 className='font-bold'> $30</h6>
-    </div>
-    
-    </>
-  ) 
+
+    const handleProductNotHovered = ()  => {
+        setProductHovered(false)  
+    }
+    return (
+        
+        <> 
+            <div className='relative'>
+                {/* render the sizes conditionality   */}   
+{/*                     
+                <div className='grid grid-cols-3'> 
+                    {productHovered && availableSizes.map((size, index) => {
+                        return <div className='bg-[#ccc] w-fit     absolute left-20  hover:bg-black' key={index} >
+                            <p className='text-[#6e6e6e]'>{size}</p>
+                        </div>
+                    })}
+                </div> */}
+            </div>     
+            <div onMouseOver={handleProuctGotHovered} onMouseLeave={handleProductNotHovered} className='relative w-[400px] m-4'>
+                <div className='bg-white p-2 rounded-full absolute left-[90%] top-3 '>
+                    <img className='w-4 h-4 lg:h-4 lg:w-4' src={heartIcon} alt="heart icon" />
+                </div>
+                <div className='bg-white p-2 rounded-full absolute left-[5%] top-3 '>
+                     <img className='w-4 h-4 lg:h-3 lg:w-3' src={addtobasket} alt="add to basket icon" />
+                </div>
+                <div className='bg-white rounded-sm p-1 absolute left-2 top-[78%]'>
+                    <p className='font-semibold text-xs'>NEW</p>
+                </div>
+                <img className='' src={womenImg} alt="a guy modeling the product" />
+                <h4 className='mt-3 text-md '>Legacy Washed Crop Top</h4>
+                <span className='text-[#6e6e6e] my-1 inline-block '> Black</span>
+                <h6 className='font-bold'> $30</h6>
+            </div>
+
+        </>
+    )
 }
 
 export default ProductIntroCard   
