008caabbf017437710a579bd481203eba632f631
badr ghanbi <bghanbi50@gmail.com>
2023-06-01 21:51:16 +0100

    create data for functional fitness section

--- src/Data.js
+++ src/Data.js
@@ -35,3 +35,115 @@ const newRealase=[
     price:"$58"
   },
 ]    
+
+
+const lifting=[
+    {
+        id:"1",
+        title:"legacy Washed Crop Top" ,
+        color:"black",
+        price:"30$"
+       },
+    {
+        id:"2",
+        title:"react tank" ,
+        color:"black",
+        price:"24$"
+       },
+    {
+        id:"3",
+        title:"legacy ruched tight shorts" ,
+        color:"mist green",
+        price:"40$"
+       },
+    {
+        id:"4",
+        title:"power washed 5 shorts" ,
+        color:"ecru brown",
+        price:"42$"
+       },    
+
+]
+
+ const RestDay=[
+    {
+        id:"1",
+        title:"minmal sports bra " ,
+        color: "aegener blue ",
+        price:"30$"
+       },    
+    {
+        id:"2",
+        title:"rest day sweats shorts" ,
+        color:"light grey core marl ",
+        price:"46$"
+       },    
+    {
+        id:"3",
+        title:"essential oversized tshirt" ,
+        color:"white ",
+        price:"26$"
+       },    
+    {
+        id:"4",
+        title:"apex7 hybrid shorts " ,
+        color:"solstice orange",
+        price:"46$"
+       },    
+ ]
+
+ const  womansBras=[
+    {
+        id:"1",
+        title:"longline sports bra " ,
+        color:"bright fuchsia ",
+        price:"38$"
+       },   
+    {
+        id:"2",
+        title:"sweat seamless longline sports bra  " ,
+        color:"dusty olive",
+        price:"48$"    
+       },   
+    {
+        id:"3",
+        title:"vital seamless  2.0 v neck sports bra " ,
+        color:"stellar blue marl",
+        price:"26.60$"
+       },   
+    {
+        id:"4",
+        title:"elevate longline sports bra " ,
+        color:"willow green ",
+        price:"44$"
+       },   
+ ]  
+
+
+
+const functionalFitness=[
+    {
+        id:"1",
+        title:"elevate longline sports bra " ,
+        color:"willow green ",
+        price:"44$"
+       },   
+    {
+        id:"4",
+        title:"elevate longline sports bra " ,
+        color:"willow green ",
+        price:"44$"
+       },   
+    {
+        id:"4",
+        title:"elevate longline sports bra " ,
+        color:"willow green ",
+        price:"44$"
+       },   
+    {
+        id:"4",
+        title:"elevate longline sports bra " ,
+        color:"willow green ",
+        price:"44$"
+       },   
+]
