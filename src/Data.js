f5c7029dbddad58a4a5f396ce6a31f8057f33991
badr ghanbi <bghanbi50@gmail.com>
2023-06-02 01:15:01 +0100

    more data scrapin

--- src/Data.js
+++ src/Data.js
@@ -36,6 +36,384 @@ const newRealase=[
   },
 ]    
 
+// product of the newrealsePage
+const newReleasePageProduct=[
+    {
+        id:'1',
+        isNew: true,
+        title:'limitless Sport Bra',
+        colors :"Black",
+        price:"$58"
+      },
+    {
+        id:'2',
+        isNew: true,
+        title:'limitless Sport Bra',
+        colors :"truffle brown",
+        price:"$58"
+      },
+    {
+        id:'3',
+        isNew: true,
+        title:'limitless crop top',
+        colors :"black/light grey",
+        price:"$50"
+      },
+    {
+        id:'4',
+        isNew: true,
+        title:'limitless Sport',
+        colors :"archived brown /truffle brown",
+        price:"48"
+      },
+    {
+        id:'5',
+        isNew: true,
+        title:'limitless cami tank ',
+        colors :"light sundried red /terracotta pink",
+        price:"$44"
+      },
+    {
+        id:'5',
+        isNew: true,
+        title:'limitless Sport Bra',
+        colors :"terracotta pink",
+        price:"$58"
+      },
+    {
+        id:'6',
+        isNew: true,
+        title:'limitless shorts',
+        colors :"sundried red/terracotta pink",
+        price:"$48"
+      },
+    {
+        id:'7',
+        isNew: true,
+        title:'limitless cami tank',
+        colors :"ligt grey/Black",
+        price:"$44"
+      },
+    {
+        id:'8',
+        isNew: true,
+        title:'limitless shorts',
+        colors :"light grey/Black",
+        price:"$48"
+      },
+    {
+        id:'9',
+        isNew: true,
+        title:'elevate spray dey leggings',
+        colors :"white/misty pink/scandi pink",
+        price:"$68"
+      },
+    {
+        id:'10',
+        isNew: true,
+        title:'legacy  washed crop top ',
+        colors :"fresh mint green/acid wash",
+        price:"$30" 
+      },
+    {
+        id:'11',
+        isNew: true,
+        title:'legacy crop top ',
+        colors :"terracotta pink/acid wash",
+        price:"$30"
+      },
+    {
+        id:'12',
+        isNew: true,
+        title:'legacy shrug top',
+        colors :"Black",
+        price:"$30"
+      },
+    {
+        id:'13',
+        isNew: true,
+        title:'legacy shrug top ',
+        colors :"truffle brown",
+        price:"$30"
+      },
+    {
+        id:'14',
+        isNew: true,
+        title:'legacy shrug top',
+        colors :"titanium grey",
+        price:"$30"
+      },
+    {
+        id:'15',
+        isNew: true,
+        title:'legacy 1/4 zip sweatshirt',
+        colors :"mist green",
+        price:"$58"
+      },
+    {
+        id:'16',
+        isNew: true,
+        title:'legacy leggings ',
+        colors :"titanium grey",
+        price:"$54"
+      },
+    {
+        id:'17',
+        isNew: true,
+        title:'legacy ruched tight shorts',
+        colors :"aegean blue",
+        price:"$40"
+      },
+    {
+        id:'18',
+        isNew: true,
+        title:'legacy leggings',
+        colors :"truffle borwn",
+        price:"$54"
+      },
+    {
+        id:'19',
+        isNew: true,
+        title:'legacy ruched tight shorts',
+        colors :"mist green",
+        price:"$40"
+      },
+    {
+        id:'20',
+        isNew: true,
+        title:'legacy washed loose shorts',
+        colors :"black",
+        price:"$36"
+      },
+    {
+        id:'21',
+        isNew: true,
+        title:'minimal sports bra ',
+        colors :"black",
+        price:"$30"
+      },
+    {
+        id:'22',
+        isNew: true,
+        title:'minimal sports bra ',
+        colors :"aegean blue ",
+        price:"$30"
+      },
+    {
+        id:'23',
+        isNew: true,
+        title:'minimal sports bra ',
+        colors :"truffle brown",
+        price:"$30"
+      },
+    {
+        id:'24',
+        isNew: true,
+        title:'crew sock 1 pk',
+        colors :"white/blue",
+        price:"$8"
+      },
+    {
+        id:'25',
+        isNew: true,
+        title:'seamless singlet',
+        colors :"black/charcoal grey",
+        price:"$74"
+      },
+    {
+        id:'26',
+        isNew: true,
+        title:'rest day slides',
+        colors :"black",
+        price:"$34"
+      },
+    {
+        id:'27',
+        isNew: true,
+        title:'rest day slides ',
+        colors :"misty pink",
+        price:"$34"
+      },
+    {
+        id:'28',
+        isNew: true,
+        title:'rest day slides',
+        colors :"core olive",
+        price:"$34"
+      },
+    {
+        id:'29',
+        isNew: true,
+        title:'rest day slides',
+        colors :"pastel green",
+        price:"$34"
+      },
+    {
+        id:'30',
+        isNew: true,
+        title:'rest day slides',
+        colors :"off white",
+        price:"$34"
+      },
+    {
+        id:'31',
+        isNew: true,
+        title:'shaker bottle ',
+        colors :"cornflower blue ",
+        price:"$14"
+      },
+    {
+        id:'32',
+        isNew: true,
+        title:'bucket hat ',
+        colors :"white ",
+        price:"$32"
+      },
+    {
+        id:'33',
+        isNew: true,
+        title:'bucket hat',
+        colors :"firefly green ",
+        price:"$32"
+      },
+    {
+        id:'34',
+        isNew: true,
+        title:'crew socks 3pk ',
+        colors :"orange/pool blue/lris green ",
+        price:"$16"
+      },
+    {
+        id:'35',
+        isNew: true,
+        title:'premium crew sock 1pk ',
+        colors :"white/black ",
+        price:"$12"
+      },
+    {
+        id:'36',
+        isNew: true,
+        title:'premium crew sock 1pk ',
+        colors :"white/dusty olive ",
+        price:"$12"
+      },
+    {
+        id:'37',
+        isNew: true,
+        title:'collegiate gymsack ',
+        colors :"dusty maroon ",
+        price:"$20"
+      },
+    {
+        id:'38',
+        isNew: true,
+        title:'collegiate gymsack ',
+        colors :"ink teal ",
+        price:"$20"
+      },
+    {
+        id:'39',
+        isNew: true,
+        title:'evreyday waist pack ',
+        colors :"black  ",
+        price:"$26"
+      },
+    {
+        id:'40',
+        isNew: true,
+        title:'evreyday waist pack  ',
+        colors :"ink teal ",
+        price:"$26"
+      },
+    {
+        id:'41',
+        isNew: true,
+        title:'lifestyle cross body ',
+        colors :"onyx grey ",
+        price:"$32"
+      },
+    {  
+        id:'42',
+        isNew: true,
+        title:'lifestyle cross body ',
+        colors :"terracotta pink ",
+        price:"$32"
+      },
+    {
+        id:'43',
+        isNew: true,
+        title:'quilted mini tote  ',
+        colors :"black ",
+        price:"$50"
+      },
+    {
+        id:'44',
+        isNew: true,
+        title:'lifestyle backpack ',
+        colors :"onyx grey ",
+        price:"$64"
+      },
+    {
+        id:'45',
+        isNew: true,
+        title:'lifestyle backpack ',
+        colors :"terraccotta pink ",
+        price:"$64"
+      },
+    {
+        id:'46',
+        isNew: true,
+        title:'quilted yoga tote ',
+        colors :"black ",
+        price:"$64"
+      },
+    {
+        id:'47',
+        isNew: true,
+        title:'quilted mini tote ',
+        colors :"frost teal ",
+        price:"$50"
+      },
+    {
+        id:'48',
+        isNew: true,
+        title:'quilted yoga tote ',
+        colors :"frost teal ",
+        price:"$64"
+      },
+    {
+        id:'49',
+        isNew: true,
+        title:'reversible bucket hat ',
+        colors :"pebble grey ",
+        price:"$30"
+      },
+    {
+        id:'50',
+        isNew: true,
+        title:'reversible bucket hat  ',
+        colors :"white/orange/peach ",
+        price:"$30"
+      },
+    {
+        id:'51',
+        isNew: true,
+        title:'handwriting sock ',
+        colors :"firefly green/white ",
+        price:"$12"
+      },
+    {
+        id:'52',
+        isNew: true,
+        title:'lifestyle barrel bag ',
+        colors :"onyx grey ",
+        price:"$72"
+      },
+
+]
+
+
+
 
 const lifting=[
     {
@@ -150,78 +528,78 @@ const functionalFitness=[
 
 const womanTrending=[
     {
-        id:1,
+        id:"1",
         title:"new relasess"
     },
     {
-        id:2, 
+        id:"2", 
         title:"matching sets"
     },
     {
-        id:3,
+        id:"3",
         title:"best sellers"
     },
     {
-        id:4,
+        id:"4",
         title:"black leggings"
     },
     {
-        id:5,
+        id:"5",
         title:"slides"
     },
     {
-        id:6,
+        id:"6",
         title:"evening blue "
     },
     {
-        id:7,
+        id:"7",
         title:"oversized"
     },
     {
-        id:8,
+        id:"8",
         title:"summer essentials"
     },
     {
-        id:9,
+        id:"9",
         title:"e-gift card"
     },
 ]
 
 const womenscollection = [
     {
-        id:1,
+        id:"1",
         title:"vital"
     },
     {
-        id:2,
+        id:"2",
         title:"adapt collection"
     },
     {
-        id:3,
+        id:"3",
         title:"sweat"
     },
     {
-        id:4,
+        id:"4",
         title:"limitless"
     },
     {
-        id:5,
+        id:"5",
         title:"essentials"
     },
     {
-        id:6,
+        id:"6",
         title:"elivate"
     },
     {
-        id:7,
+        id:"7",
         title:"legacy"
     },
     {
-        id:8,
+        id:"8",
         title:"gs power"
     },
     {
-        id:9,
+        id:"9",
         title:"key fits "
     },
    
@@ -305,3 +683,76 @@ const mensCollection=[
          title:"315 collection"
     },
 ] 
+
+const help=[
+    {
+        id:'1',
+         title:"faq"
+    },
+    {
+        id:'2',
+         title:"delivery information"
+    },
+    {
+        id:'3',
+         title:"returns policy"
+    },
+    {
+        id:'4',
+         title:"make a return "
+    },
+    {
+        id:'5',
+         title:"orders"
+    },
+    {
+        id:'6',
+         title:"submit a fake"
+    },
+]
+
+const myaccount=[
+    {
+        id:'1',
+         title:"login"
+    },
+    {
+        id:'2',
+         title:"register"
+    },
+]
+
+const pages=[
+    {
+        id:'1',
+         title:"gymshark central"
+    },
+    {
+        id:'2',
+         title:"about us "
+    },
+    {
+        id:'3',
+         title:"careers"
+    },
+    {
+        id:'4',
+         title:"student discount"
+    },
+    {
+        id:'7',
+         title:"accessibility statement"
+    },
+    {
+        id:'8',
+         title:"factory list"
+    },
+    {
+        id:'9',
+         title:"sustainable "
+    },
+    {
+        id:'6',
+         title:"submit a fake"
+    },  
+]
