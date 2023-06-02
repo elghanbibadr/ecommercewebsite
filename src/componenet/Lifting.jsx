de70ae5c266bdf4dd285b44162429e64db2d32a0
badr ghanbi <bghanbi50@gmail.com>
2023-06-02 13:25:31 +0100

    add the view all link to the lifting section

--- src/componenet/Lifting.jsx
+++ src/componenet/Lifting.jsx
@@ -4,15 +4,21 @@ import ProductIntroCard from './UI/ProductIntroCard'
 
 const Lifting = () => {
     return (
-        <>
-            <h1 className='font-semibold p-6 mt-10 text-3xl'>LIFTING </h1>
+        <div className='p-6'>
+               <div className='flex justify-between items-center'>
+                <div>
+                    <h1 className='font-semibold mt-16 text-3xl'>LIFTING  </h1>                </div>
+                <div className='self-end'>
+                    <a className='text-black font-bold underline' href="">View all</a>
+                </div>
+            </div>   
 
             <div className="grid  grid-cols-1  md:grid-cols-2 lg:grid-cols-4" >
                 {lifting.map(({ id, title, productImg, color, isNew, price }) => {
                     return <ProductIntroCard key={id} isNew={isNew} title={title} colors={color} productImg={productImg} price={price} />
                 })}
             </div>
-        </>
+        </div>
     )
 }
 
--- src/componenet/RestDay.jsx
+++ src/componenet/RestDay.jsx
@@ -4,14 +4,20 @@ import ProductIntroCard from './UI/ProductIntroCard'
 const RestDay = () => {
 
     return  (
-        <>
-            <h1 className='font-semibold p-6 mt-16 text-3xl'>REST DAY </h1>
+        <div className="p-6">
+             <div className='flex justify-between items-center'>
+                <div>
+                    <h1 className='font-semibold mt-16 text-3xl'>REST DAY </h1>                </div>
+                <div className='self-end'>
+                    <a className='text-black font-bold underline' href="">View all</a>
+                </div>
+            </div>   
             <div className="grid  grid-cols-1  md:grid-cols-2 lg:grid-cols-4" >
                 {restDay.map(({ id, title, productImg, color, isNew, price }) => {
                     return <ProductIntroCard key={id} isNew={isNew} title={title} colors={color} productImg={productImg} price={price} />
                 })}
             </div>
-        </>
+        </div>
     )
 }
 
