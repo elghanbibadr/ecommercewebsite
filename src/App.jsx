c260c26080dd3d9d2a8bf652f0f53f372fb265dc
badr ghanbi <bghanbi50@gmail.com>
2023-06-02 12:51:55 +0100

    clean up the app compoenent for unused componenet

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './componenet/Navbar'
 import Hero from './componenet/Hero'
 import { NewRelease } from './componenet/NewRelease'
 import PurcePerformanceIntro from './componenet/PurcePerformanceIntro'
-import ProductIntroCard from './componenet/UI/ProductIntroCard'
 import RestDayIntro from './componenet/RestDayIntro'
 import Lifting from './componenet/Lifting'
 const App = () => {
--- src/componenet/RestDayIntro.jsx
+++ src/componenet/RestDayIntro.jsx
@@ -3,27 +3,25 @@ import restDayVideo from "../assets/ecomVideo.mp4"
 import Btn from './UI/Btn'
 
 const RestDayIntro = () => {
-  return (
+    return (
 
-   <>
-   
-    {/* <div className="overlay"></div> */}
-    <div className='relative mt-20 h-[500px]'>
-    <video src={restDayVideo} autoPlay loop muted />
-    {/* <div className="content p-4 lg:p-10"> */}
-    
-    <div className='absolute text-white top-52 left-10 '>
-        <h2 className=' text-2xl lg:text-4xl font-medium'>OUR REST DAY ‘FITS</h2>
-            <p className='my-3  text-sm lg:text-md font-medium'>Live your next rest day in comfort and style.</p>
-            <div className='flex items-center w-full right-2'>
-                <Btn className="bg-white mx-2 w-full sm:w-auto text-black" text="shop women" />
-                     <Btn className="bg-[#53565a] hidden lg:block text-white" text="shop men" />
+        <>
+            {/* we need to replace t he current video with another one  */}
+
+            <div className='relative mt-20 h-[500px]'>
+                <video src={restDayVideo} autoPlay loop muted />
+                <div className='absolute text-white top-52 left-10 '>
+                    <h2 className=' text-2xl lg:text-4xl font-medium'>OUR REST DAY ‘FITS</h2>
+                    <p className='my-3  text-sm lg:text-md font-medium'>Live your next rest day in comfort and style.</p>
+                    <div className='flex items-center w-full right-2'>
+                        <Btn className="bg-white mx-2 w-full sm:w-auto text-black" text="shop women" />
+                        <Btn className="bg-[#53565a] hidden lg:block text-white" text="shop men" />
+                    </div>
+                </div>
             </div>
-    </div>
-    </div>
-   
-   </>
-  )
+
+        </>
+    )
 }
 
 export default RestDayIntro
